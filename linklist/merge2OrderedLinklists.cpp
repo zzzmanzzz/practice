caa27a17db2ce30d07cd488b9f34dfaadc538d6b
wyatt huang <wyatth@gmail.com>
2020-08-13 14:47:00 +0800

    init merge 2 ordered linklists

---
+++ linklist/merge2OrderedLinklists.cpp
@@ -0,0 +1,16 @@
+#include"node.hpp"
+
+/*
+merge兩個有序link list
+Reference Book 1, 1.9
+*/
+
+int main() {
+    int arr1[] = {1, 4, 6, 7, 8};
+    int arr2[] = {-10, -5, 5, 10, 15};
+    node* head1 = initViaArray(arr1, 5);
+    node* head2 = initViaArray(arr2, 5);
+
+
+    return 0;
+}
